4d353003ced1e8a3f58849003611b513f06ddce4
Marcin Widz <Marcinwidz91@gmail.com>
2021-07-28 15:24:49 +0200

    Tom

--- src/Containters/Home.js
+++ src/Containters/Home.js
@@ -1,6 +1,18 @@
 import Header from "../Components/Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get();
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+  });
+
   return (
     <div>
       <div className='hero'>
